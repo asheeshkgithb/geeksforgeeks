93ddc61432e4cbed960c2b4576c4f2eee89e022c
asheeshkgithb <asheesh.kumar.cs28@iilm.edu>
2026-09-16 18:05:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Taking Input/taking-input.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    int n;
+    float f;
+    int ff;
+
+    getline(cin, s);
+    cin >> n;
+    cin >> f;
+
+    ff = f;
+
+    cout << s << endl;
+    cout << n << endl;
+    cout << ff << endl;
+
+    return 0;
+}
