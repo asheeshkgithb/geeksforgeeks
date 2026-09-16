cb038590a265e998e9291a32296400b73feb6f87
asheeshkgithb <asheesh.kumar.cs28@iilm.edu>
2026-09-16 18:20:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decision Making/decision-making.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if (n < m)
+        cout << "less" << endl;
+    else if (n == m)
+        cout << "equal" << endl;
+    else
+        cout << "greater" << endl;
+
+    return 0;
+}
